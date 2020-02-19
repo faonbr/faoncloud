1497df10780703a6ebf29dadb084f5f7c184e2c8
FAONbr <faonbr@gmail.com>
2020-02-19 17:11:19 -0300

    logo

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -1,23 +1,4 @@
 <template>
-  <div class="logo-wrapper">
-    <div
-      :class="{
-        'logo-inner': true,
-        blink: blinking
-      }"
-    >
-      <img
-        v-for="x in frames"
-        :key="x"
-        :src="`/logo-frames/logo-f${x - 1}.svg`"
-      />
-      <img
-        v-for="x in frames"
-        :key="`${x}-r`"
-        :src="`/logo-frames/logo-f${frames - x}.svg`"
-      />
-    </div>
-  </div>
   <div>
   <img src="/faoncloudlogo.jpg">
   </div>
