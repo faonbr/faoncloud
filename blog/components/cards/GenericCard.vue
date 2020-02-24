d32d9bdadb581dff5a57e34255c9d4ce671a3a7c
FAONbr <faonbr@gmail.com>
2020-02-24 19:18:46 -0300

    size fix

--- blog/components/cards/GenericCard.vue
+++ blog/components/cards/GenericCard.vue
@@ -64,7 +64,7 @@ export default {
         .toLowerCase()
         .split('x')
         .map((size) => {
-          return size * 2000
+          return size * 200
         })
     },
     responsiveImage() {
