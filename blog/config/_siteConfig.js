4c4e02800301ad321f04a4a406468ea18c57ba4b
FAONbr <faonbr@gmail.com>
2020-02-25 22:36:04 -0300

    a

--- blog/config/_siteConfig.js
+++ blog/config/_siteConfig.js
@@ -71,7 +71,7 @@ export default {
     // Can be the form action on a mail chimp form, a hubspot form,
     // or any other url you want to post the form data to
     mailchimp: {
-      on: true,
+      on: false,
       formAction:
         'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
     },
