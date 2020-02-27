45fd7b48ecfab0994ae257ae43fb4db827bc6485
FAONbr <faonbr@gmail.com>
2020-02-27 01:30:32 -0300

    indexes vue updates

--- blog/pages/categories/index.vue
+++ blog/pages/categories/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="categories-page" class="page-wrapper categories-page">
-    <site-hero title="Categories" image="https://picsum.photos/1800/1801" />
+    <site-hero title="Categories" image="/cloud4freelogo.jpg" />
     <main-section theme="one-column">
       <categories-grid />
     </main-section>
--- blog/pages/index.vue
+++ blog/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage"/>
+    <site-hero :title="title" :subtitle="subtitle" image="/clouds.jpg"/>
     <main-section theme="one-column">
       <template v-slot:default>
         <!-- All Posts -->
